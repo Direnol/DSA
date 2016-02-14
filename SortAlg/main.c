262b8d82bafbb768dafacedc5a227509bae7ebb5
Valery Agishev <phaseowner@gmail.com>
2016-02-14 22:58:52 +0600

    Update main.c

--- SortAlg/main.c
+++ SortAlg/main.c
@@ -2,5 +2,11 @@
 
 int main(int argc, char * argv[])
 {
-	return EXIT_SUCCESS;
+    if(argc < 2)
+    {
+        printf("Missing array size!");
+        return EXIT_FAILURE;
+    }
+        
+    return EXIT_SUCCESS;
 }
