e1d3625d536701cbd5ce7167ffc3dda14bede18c
Valery <phaseowner@gmail.com>
2016-02-15 08:33:07 +0600

    Рандомное заполнение динамического массива

--- SortAlg/main.c
+++ SortAlg/main.c
@@ -7,6 +7,14 @@ int main(int argc, char * argv[])
         printf("Missing array size!");
         return EXIT_FAILURE;
     }
-        
+
+    int array_size = atoi(argv[1]);
+    int * array = (int *)malloc(sizeof(int)*array_size);
+    for(int i=0; i < array_size; i++)
+	array[i] = getrand(1, 100001);
+
+
+    free(array);
+
     return EXIT_SUCCESS;
 }
