5cb984613f407adb74488e7e365be3cf85eca9b2
Direnol <direnol@yandex.ru>
2016-03-12 18:40:07 +0600

    Some modif

--- sort.c
+++ sort.c
@@ -3,10 +3,10 @@
 #include <stdint.h>
 #include <sys/time.h>
 
-void countingsort (uint32_t *mass, int size);
-void quicksort (uint32_t *mas, int first, int last);
-void bubblesort (uint32_t mass[], int size);
-int getrand (int min, int max);
+void CountingSort(int *array, int size);
+void QuickSort(uint32_t *mas, int first, int last);
+void BubbleSort(uint32_t mass[], int size);
+int getrand(int min, int max);
 double wtime();
 
 int main (int argc, char * argv[])
@@ -15,122 +15,126 @@ int main (int argc, char * argv[])
     double t;
     FILE * ptr;
 
-    if (argc < 2){
-        printf ("Missing array size!");
+    if (argc < 2) {
+        printf("Missing array size!");
         return EXIT_FAILURE;
     }
 
-    if (argc < 3){
+    if (argc < 3) {
         printf ("Not number sort!");
         return EXIT_FAILURE;
     }
 
-    int array_size = atoi(argv[1]), s = atoi(argv[2]);
+    int array_size = atoi(argv[1]);
+    int s = atoi(argv[2]);
 
-    if ((s != 1) && (s != 2) && (s !=3 )){
-        printf ("No correct number sort!");
+    if ((s != 1) && (s != 2) && (s !=3 )) {
+        printf("No correct number sort!");
         return EXIT_FAILURE;
     }
 
-    uint32_t * array = malloc(sizeof(uint32_t) * array_size);
+    uint32_t *array = malloc(sizeof(uint32_t) * array_size);
 
-    if (array == NULL){
-        printf ("Error of memory allocation!\n");
+    if (array == NULL) {
+        printf("Error of memory allocation!\n");
         return EXIT_FAILURE;
     }
 
-    for(i = 0; i < array_size; i++){
+    for(i = 0; i < array_size; i++) {
         array[i] = getrand(1, 100001);
-        //printf("array[%d] = %d\n",i,array[i]);
+        // printf("array[%d] = %d\n", i, array[i]);
     }
 
-    if (s == 1){
+    if (s == 1) {
         t = wtime();
-        quicksort(array,0,array_size-1);
+        QuickSort(array, 0, array_size - 1);
         t = wtime() - t;
-        ptr = fopen("qsort.dat","a");
-    } else if (s == 2){
+        ptr = fopen("qsort.dat", "a");
+    } else if (s == 2) {
         t = wtime();
-        bubblesort(array,array_size);
+        BubbleSort(array, array_size);
         t = wtime() - t;
-        ptr = fopen("bsort.dat","a");
-    } else if (s == 3){
+        ptr = fopen("bsort.dat", "a");
+    } else if (s == 3) {
         t = wtime();
-        countingsort(array,array_size);
+        CountingSort(array, array_size);
         t = wtime()- t;
-        ptr = fopen("csort.dat","a");
+        ptr = fopen("csort.dat", "a");
     }
 
-    fprintf (ptr, "%d\t%.6f\n", array_size, t);
-    fclose (ptr);
+    fprintf(ptr, "%d\t%.6f\n", array_size, t);
+    fclose(ptr);
 
-    /*for (i = 0; i < array_size; i++)
-        printf("a[%d] = %d\n",i,array[i]);*/
+    /* for (i = 0; i < array_size; i++)
+        printf("a[%d] = %d\n", i, array[i]); */
 
     free(array);
     return EXIT_SUCCESS;
 }
 
 
-void countingsort(uint32_t *A, int n)
+void CountingSort(int *array, int size)
 {
-    int i, min = A[0], max = A[0];
+    int i;
+    int max = array[0];
 
-    for (i = 1; i < n; i++){
-        if (A[i] < min)
-            min = A[i];
-        else
-        if (A[i] > max)
-            max = A[i];
+    for (i = 1; i < size; i++) {
+        if (array[i] > max)
+            max = array[i];
     }
 
-    uint32_t *C = (uint32_t*)malloc(sizeof(uint32_t)*(max-min+1));
-    uint32_t *B = (uint32_t*)malloc(sizeof(uint32_t)*n);
+    int K = max;
 
-    for (i = 0; i < (max-min+1); i++)
+    int *C = (int *) malloc(sizeof(int) * (K + 1));
+    int *B = (int *) malloc(sizeof(int) * size);
+
+    for (i = 0; i <= K; i++)
         C[i] = 0;
 
-    for (i = 0; i<n; i++)
-        C[ A[i]-min ]++;
+    for (i = 0; i < size; i++) {
+        C[array[i]]++;
+    }
 
-    for (i = 1; i < (max-min+1); i++)
-        C[i] += C[i-1];
+    for (i = 1; i <= K ; i++)
+        C[i] += C[i - 1];
 
-    for (i = 0; i < n; i++)
-        B[ C[ A[i]-min ]-- -1 ] = A[i];
+    for (i = (size - 1); i >= 0; i--) {
+        C[array[i]]--;
+        B[C[array[i]]] = array[i];
+    }
 
-    for (i = 0; i < n; i++)
-        A[i] = B[i];
+    for (i = 0; i < size; i++)
+        array[i] = B[i];
 
-    free (B);
-    free (C);
+    free(C);
+    free(B);
 }
-void quicksort(uint32_t *mas, int first, int last)
+
+void QuickSort(uint32_t *mas, int first, int last)
 {
-    int mid, count;
+    int mid, tmp;
     int f = first, l = last;
-    mid = mas[(f+l) / 2]; //вычисление опорного элемента
-    do
-    {
+    mid = mas[(f + l) / 2]; // вычисление опорного элемента
+    do {
         while (mas[f] < mid)
             f++;
         while (mas[l] > mid)
             l--;
-        if (f <= l){ //перестановка элементов
-            count = mas[f];
+        if (f <= l) { // перестановка элементов
+            tmp = mas[f];
             mas[f] = mas[l];
-            mas[l] = count;
+            mas[l] = tmp;
             f++;
             l--;
         }
     } while (f < l);
     if (first < l)
-        quicksort(mas, first, l);
+        QuickSort(mas, first, l);
     if (f < last)
-        quicksort(mas, f, last);
+        QuickSort(mas, f, last);
 }
 
-void bubblesort(uint32_t arr[], int size)
+void BubbleSort(uint32_t arr[], int size)
 {
     int tmp, i, j;
 
