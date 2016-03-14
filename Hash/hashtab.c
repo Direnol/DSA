e4328dc1f98ed35cf732806756ab74c8a1529606
Valery Agishev <phaseowner@gmail.com>
2016-03-14 11:15:32 +0600

    Прототипы 2х библиотек, для реализации в соответствии с заданием

---
+++ Hash/hashtab.c
@@ -0,0 +1,26 @@
+#include "hashtab.h"
+
+unsigned int hashtab_hash(char *key)
+{
+
+}
+
+void hashtab_init(struct listnode **hashtab)
+{
+
+}
+
+void hashtab_add(struct listnode **hashtab, char *key, int value)
+{
+    
+}
+
+struct listnode *hashtab_lookup(struct listnode **hashtab, char *key)
+{
+    
+}
+
+void hashtab_delete(struct listnode **hashtab, char *key)
+{
+    
+}
